d2eb0ab17ff45f1469ad4ef1ebff181a0640c861
Oscar Söderlund <oscar@nebulr.group>
2022-04-05 20:22:22 +0200

    Dev container

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## This file setups all prerequisites for the docker container, used both dev environment and in CI/CD pipeline.
+## For prerequisites strictly related to dev environment see ./.devcontainer/init.sh which is run before this script.
+echo "Installing dependencies..."
+
+sudo npm install
+
+echo "Done initializing"
