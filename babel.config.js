c3f98acd909cb9ba50bf4887aeef59b36e2b2776
Oscar Söderlund <oscar@nebulr.group>
2022-06-25 13:51:08 +0200

    Resolved SyntaxError problems Problem was due to @babel/plugin-transform-typescript in babel.config.js. Disableing it resolved the issue. Most probably this is due to a version missmatch that was introduced by codegen package

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,13 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    "plugins": [
-      [
-        "@babel/plugin-transform-typescript",
-        {
-          "allowDeclareFields": true
-        }
-      ]
-    ]
+    // "plugins": [
+    //   [
+    //     "@babel/plugin-transform-typescript",
+    //     {
+    //       "allowDeclareFields": true
+    //     }
+    //   ]
+    // ]
   };
 };
